cc59fbf319dc511cd4776b39ed3be96ac72c4936
william <william810212@gmail.com>
2021-06-10 01:55:35 +0800

    feat: add deploy github pages

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/liang/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '/src')
+    }
+  },
   plugins: [vue()]
 })
