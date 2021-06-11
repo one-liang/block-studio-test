c96b4b0712cee96d7237b4d655a99dfd383aa558
william <william810212@gmail.com>
2021-06-11 16:53:29 +0800

    chore: add custom

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ module.exports = {
   purge: ['./index.html', './src/**/*.{vue,js}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      borderWidth: {
+        '16': '16px',
+      }
+    },
     fontFamily: {
       'sans': ['Ubuntu', 'sans-serif']
     },
