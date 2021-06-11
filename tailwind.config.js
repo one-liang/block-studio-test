f020e57989f1ced39f1558f956c244fbebb0e3f7
william <william810212@gmail.com>
2021-06-11 13:51:00 +0800

    chore: custom config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
+  mode: 'jit',
   purge: ['./index.html', './src/**/*.{vue,js}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      'sans': ['Ubuntu', 'sans-serif']
+    },
+    screens: {
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+    }
   },
-  variants: {
-    extend: {},
-  },
+  variants: {},
   plugins: [],
 }
