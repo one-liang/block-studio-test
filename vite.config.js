292aa9e222b5a1626a736486a7dfb213ffcf8973
william <william810212@gmail.com>
2021-06-11 14:01:22 +0800

    chore: update path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/block-studio-test/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '/src')
+    }
+  },
   plugins: [vue()]
 })
